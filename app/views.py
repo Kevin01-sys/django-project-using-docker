87d0512798b61dffe440cfd2338bc1b20a8798d5
Kevin Duoc <kevinchocano@gmail.com>
2022-04-06 23:11:01 -0400

    Ajax of form id_contact_form is working

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.template.loader import render_to_string
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils import timezone
@@ -15,61 +16,28 @@ from app.forms import ContactForm
 class HomeView(TemplateView):
     template_name = 'manta/home.html'
 
+    """as its name suggests, obtains the data from the context. 
+    in this case, the ContactForm() form is passed to it."""
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['contact_form'] = ContactForm()
 
         return context
-
+    # when a POST request is made, this function is executed
     def post(self, request, *args, **kwargs):
+
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
 
-        body = render_to_string(
-            'manta/email_content.html', {
-                'name': name,
-                'email': email,
-                'message': message,
-            },
-        )
-
-        #email_message = EmailMessage(
-        #    subject='Mensaje de usuario',
-        #    body=body,
-        #    from_email=email,
-        #    to=['kevinchocano@gmail.com'],
-        #)
-        #email_message.content_subtype = 'html'
-        #email_message.send()
-
-        #EmailMessage() envia el correo usando las librerias de Python y le indicas el server smtp al que debe conectar
-        #message = EmailMessage()
-        #message.set_content("test message sent locally")
-        #message.set_content(body)
-        #message['Subject'] = 'Your subject here'
-        #message['From'] = 'kevinchocano@gmail.com'
-        #message['To'] = 'kevinchocano@gmail.com'
-        # Para activar el servidor de prueba es con el comando // python -m smtpd -n -c DebuggingServer localhost:1025
-        #smtp_server = smtplib.SMTP('smtp.server.address:587') // smtp_server = smtplib.SMTP('smtp.server.address:25') // smtp_server = smtplib.SMTP('aspmx.l.google.com:25') 
-        #smtp_server = smtplib.SMTP('localhost:1025')
-        #smtp_server.send_message(message)
-        #smtp_server.quit()
-        # send_mail envia el correo usando librerias de Django
-        send_mail(
-            'Subject here',
-            body,
-            'kevinchocano@gmail.com',
-            ['kevinchocano@gmail.com'],
-            fail_silently=False,
-        )
-
-        # Se crea un Json que son los datos que enviara por ajax
+        # A Json is created with the data to be displayed on the screen.
         return JsonResponse(
             {
                 'content': {
                     'message': 'Mensaje enviado correctamente',
                 },
-                'name': name
+                'name': name,
+                'email': email,
+                'message': message
             }
         )
